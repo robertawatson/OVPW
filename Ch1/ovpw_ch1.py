a2675ff3b52f23d2d07cab8a761df0f8800fecb0
robertawatson <robertallanwatson@gmail.com>
2021-03-10 14:41:50 -0600

    start

--- Ch1/ovpw_ch1.py
+++ Ch1/ovpw_ch1.py
@@ -3,15 +3,28 @@
 # Contract Settlement & Cashflow
 
 import pandas as pd #pylint: disable=E0401
+import numpy as np #pylint: disable=E0401
 
-df = pd.DataFrame(columns = ['Trade Sequence','Stock Price','Trade','Open Position Share','Cash Flow','Cumulative Cash Flow','Realized P&L','Unrealized P&L','Total Cumulative P&L'])
+# df = pd.DataFrame(columns = ['Trade Sequence','Stock Price','Trade','Open Position Share','Cash Flow','Cumulative Cash Flow','Realized P&L','Unrealized P&L','Total Cumulative P&L'])
+data = pd.read_csv("ch1_data.csv")
+w = str("This is a simple calculator for \n Options and Volatility Pricing - Chapter 1 of \n Contract Settlement and Cashflow")
+data.head()
+
 def main():
     pass
 
+def welcome():
+    print(w)
 
+def calc():
+    pass
+    
+def data(): # this may not be needed
+    pass
 
-
-
+def readout(): 
+    print()
+    pass
 
 
 if __name__ == "__main__":
